53d63a6d59f456a7f80f3ccb151422acf6f731ea
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-05-01 19:48:22 -0700

    Update SkillDemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -2,6 +2,6 @@ public class SkillDemo {
     public static int multiply(int num1, int num2) {
         int multipledInt;
         multipledInt = num1 * num2;
-        return 0;
+        return multipledInt;
     }
 }
